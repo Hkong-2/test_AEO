2d9fe851edacb31ff3aeab8f906edd18142c961f
hqhuy22 <hqhuy22@clc.fitus.edu.vn>
2026-03-21 23:38:38 +0700

    feat: article management & creation

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -3,9 +3,10 @@ import 'package:boilerplate/presentation/home/home.dart';
 import 'package:boilerplate/presentation/login/login.dart';
 import 'package:boilerplate/presentation/register/register.dart';
 import 'package:boilerplate/presentation/dashboard/dashboard.dart';
-import 'package:boilerplate/presentation/home/home.dart';
-import 'package:boilerplate/presentation/login/login.dart';
 import 'package:boilerplate/presentation/overview/overview.dart';
+import 'package:boilerplate/presentation/all_posts/all_posts_screen.dart';
+import 'package:boilerplate/presentation/ai_writer/ai_writer_screen.dart';
+import 'package:boilerplate/presentation/auto_generation/auto_generation_screen.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
@@ -20,6 +21,9 @@ class Routes {
   static const String home = '/post';
   static const String overview = '/overview';
   static const String analytic = '/analytic';
+  static const String allPosts = '/all-posts';
+  static const String aiWriter = '/ai-writer';
+  static const String autoGeneration = '/auto-generation';
 
   static final routes = <String, WidgetBuilder>{
     dashboard: (BuildContext context) => DashboardScreen(),
@@ -28,6 +32,9 @@ class Routes {
     forgotPassword: (BuildContext context) => ForgotPasswordScreen(),
     home: (BuildContext context) => HomeScreen(),
     overview: (BuildContext context) => OverviewScreen(),
+    allPosts: (BuildContext context) => AllPostsScreen(),
+    aiWriter: (BuildContext context) => AiWriterScreen(),
+    autoGeneration: (BuildContext context) => AutoGenerationScreen(),
     //analytic: (BuildContext context) => AnalyticScreen(),
   };
 }
